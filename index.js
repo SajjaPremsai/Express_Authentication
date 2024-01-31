56ca8d5e94bbb261755689a17b45d4a0db449a3b
SajjaPremsai <sajjapremsai8938@gmail.com>
2024-01-31 11:55:40 +0530

    Changed the origin details

--- index.js
+++ index.js
@@ -12,15 +12,11 @@ const app = express()
 
 app.use(bodyparser.json())
 app.use(cors({
-    origin: 'https://express-authentication-client.onrender.com',
-    credentials : true
-}))
-app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://express-authentication-client.onrender.com', 'http://localhost:3000'],
     credentials: true
 }));
 
-app.options('*',cors())
+app.options('*', cors());
 
 app.use(cookieParser())
 
